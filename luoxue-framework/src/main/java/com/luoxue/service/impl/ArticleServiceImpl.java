30faa56cee8e96c4e393e0553a60ef34fcec0f15
luo xue <2426296869@qq.com>
2024-10-27 21:15:20 +0800

    updata

--- luoxue-framework/src/main/java/com/luoxue/service/impl/ArticleServiceImpl.java
+++ luoxue-framework/src/main/java/com/luoxue/service/impl/ArticleServiceImpl.java
@@ -3,12 +3,17 @@ package com.luoxue.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.luoxue.constants.SystemConstants;
 import com.luoxue.domin.ResponseResult;
 import com.luoxue.domin.entity.Article;
+import com.luoxue.domin.vo.HotArticleVo;
 import com.luoxue.mapper.ArticleMapper;
 import com.luoxue.service.ArticleService;
+import com.luoxue.utils.BeanCopyUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,9 +23,16 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> impl
     public ResponseResult hotArticle() {
         LambdaQueryWrapper<Article> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.orderByDesc(Article::getViewCount);
-        queryWrapper.eq(Article::getStatus,0);
+        queryWrapper.eq(Article::getStatus, SystemConstants.ARTICLE_STATUS_NORMAL);
         Page<Article> articlePage = this.page(new Page<>(1, 10), queryWrapper);
         List<Article> articles = articlePage.getRecords();
-        return ResponseResult.okResult(articles);
+        /*List<HotArticleVo> hotArticleVos = new ArrayList<>();
+        for (Article article : articles) {
+            HotArticleVo vo = new HotArticleVo();
+            BeanUtils.copyProperties(article,vo);
+            hotArticleVos.add(vo);
+        }*/
+        List<HotArticleVo> hotvos = BeanCopyUtils.beanCopyList(articles, HotArticleVo.class);
+        return ResponseResult.okResult(hotvos);
     }
 }
